9fd89b86d30e8cdbbeb02592286a99328ca6a413
General Assembly <generalassembly@Generals-MacBook-Air-3.local>
2016-04-01 18:45:56 -0700

    added some CSS and HTML.  Tweaked battle mechs

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@ console.log('Hey');
 
 //  Screen 1: home page with the title on it.  Needs to have a some sort of picture, and a "click to start" button.
 
-//  Screen 2: screen will be an introduction to the story, and how we ended up needing a noble hero.  
+//  Screen 2: screen will be an introduction to the story, and how we ended up needing a noble hero.
 //  It will be a window on a black BG.  The window will be slightly transparent.
 //  Page will have a skip button and next button (if story needs to progress to another page)
 
-//  Screen 3: Class selection screen.  Select your class from 3 - Warrior, Dwarf and Mage.  Need to create the 
+//  Screen 3: Class selection screen.  Select your class from 3 - Warrior, Dwarf and Mage.  Need to create the
 //  following here:
 //  Constructors for each class
 //  Description of each class, along with strengths and weaknesses - numerical value, out of 100
@@ -18,7 +18,7 @@ console.log('Hey');
 //     Screen Transitions
 // ***************************
 
-$(document).ready(function(){ 
+$(document).ready(function(){
   $('#startText').click(function() {
     $('.homePage').hide();
     $('.charSelect').show();
@@ -31,8 +31,8 @@ $(document).ready(function(){
     $('.charSelect').hide();
     $('.storyPage').show();
     console.log('Clicked');
-  });  
-   
+  });
+
   $('#storyContinue').click(function() {
     $('.textContainer').hide();
     $('#storyContinue').hide();
@@ -41,7 +41,7 @@ $(document).ready(function(){
     $('#begin').show();
   });
 
-        
+
   $('#begin').click(function() {
     $('.tutPage').hide();
     $('#begin').hide();
@@ -50,13 +50,16 @@ $(document).ready(function(){
     // $('.battleScreen').hide();
     $('.battleScreen').show();
   });
-  
 
-// Function for highlighting only one div 
+
+// Function for highlighting only one div
   $('.character').click(function(){
     $('.character').removeClass('active');
     $(this).addClass('active');
+
   });
+
+
 
 
   var warriorSelect = $('#warriorDiv').data(warrior);
@@ -64,7 +67,7 @@ $(document).ready(function(){
   $('#dwarfDiv').data(dwarf);
 
 
-  
+
     // $(this).attr('id');
     // console.log($(this).attr('id'));
 
@@ -72,7 +75,7 @@ $(document).ready(function(){
 
   // $('body').on('keypress', function(event){
   //   switch(event.which){
-  //     case 119 : 
+  //     case 119 :
   //       console.log('W');
   //       break;
   //     case 97 :
@@ -82,13 +85,13 @@ $(document).ready(function(){
   //     case 115 :
   //       console.log('S');
   //       break;
-  //     case 100 : 
+  //     case 100 :
   //       console.log('D');
   //       moveRight();
   //       break;
   //     default :
   //       break;
-  //   } 
+  //   }
 
   // });
   // var moveLeft = function(){
@@ -105,9 +108,9 @@ $(document).ready(function(){
 
 
 
-// This is where the character needs to be assigned to the canvas element 
+// This is where the character needs to be assigned to the canvas element
 
-//  Screen 4: Another story screen.  Where and what the Hero needs to accomplish to finish level.  
+//  Screen 4: Another story screen.  Where and what the Hero needs to accomplish to finish level.
 //  Skip button and begin button
 //  Window on black BG, semi transparent
 
@@ -127,7 +130,7 @@ $(document).ready(function(){
   //     radius: 100,
   //     sides: 3
   //   });
-  // }; 
+  // };
 
   // $('fight').click(function(){
   //   impAtk();
@@ -177,7 +180,7 @@ $(document).ready(function(){
 //       var rand = Math.round(Math.random(500));
 //       enemyDead = setTimeout(function() {
 //               generateEnemy();
-//               // loop();  
+//               // loop();
 //       }, rand);
 //   }());
 // });
@@ -210,7 +213,7 @@ $(document).ready(function(){
   //     warrior.dexterity=11;
   //     warrior.experience=200; // this needs to equal the value attained
   //   }
-  // };  
+  // };
 
   // var rogueExpUp = function(){
   //   if(rogue.experience >= 200){
@@ -219,7 +222,7 @@ $(document).ready(function(){
   //     rogue.dexterity=16;
   //     rogue.experience=200;
   //   }
-  // };  
+  // };
 
   // var dwarfExpUp = function(){
   //   if(dwarf.experience >= 200){
@@ -228,7 +231,7 @@ $(document).ready(function(){
   //     dwarf.dexterity=12;
   //     dwarf.experience=200;
   //   }
-  // };  
+  // };
 
   // var monsterArray = [imp, pitfiend];
 
@@ -251,7 +254,7 @@ $(document).ready(function(){
 //  added to the heros inventory.  What I need to create:
 //	Fight mechanic - turn based, using D&D dice rolling logic, including weapon and armour buffers
 //  Monsters - Constructors that generate randomly, depending on characters level.  Their inventory will also be
-//	goverened by their level (in terms of what they are carrying.)  Monster class and therefore inventory will be 
+//	goverened by their level (in terms of what they are carrying.)  Monster class and therefore inventory will be
 //  arrays where the items are inserted (if else statements) that will then append to the hero class.
 //	Experience upgrade - Constructor product gets a percentage increase defined by its class.  ie, some attributes
 //  will increase better than others, dependent on the selected class.
@@ -260,39 +263,39 @@ $(document).ready(function(){
 //	Screen 7: Battle Screen.  Will be turn based where player clicks button, gets result, and then monster attacks
 //  and result is displyed.  Health bar decreases accordingly
 
-//	Screen 8: Monster defeat screen.  Semi-transparent window with main play screen in the BG.  Window lists how 
+//	Screen 8: Monster defeat screen.  Semi-transparent window with main play screen in the BG.  Window lists how
 //  much experience was gained, as-well-as inventory items (if any) were gained.  Programming needed:
 //	Accept button
 //	If inventory is full - Mew window pops up asking which items player wishes to take and discard.
 
-//  Screen 9:  Inventory - 5x5 grid of space available.  Pictures of item (weapon/armour/money) will show up in the 
+//  Screen 9:  Inventory - 5x5 grid of space available.  Pictures of item (weapon/armour/money) will show up in the
 //  space when it is full.  This screen will require the following:
 //	Table - inventroy display
 //	Accept and Discard button.  If items are selected and accept button is clicked, nothing happens and player is
 //	returned to game screen.  If items are selected, and player clicks discard, confirmation screen pops up:
-//		Discard and cancel buttons.  If discard is clicked, then items are removed from inventory.  If cancel is 
+//		Discard and cancel buttons.  If discard is clicked, then items are removed from inventory.  If cancel is
 //		clicked then screen reverts back to inventroy screen, with current items selected.
 
-//  Screen 10:  Random item pickup.  This will be if the player finds a chest on the journey.  This would be an 
-//	array, triggered by a randomiser.  Chests will not appear in the same place.  The items that they contain will 
+//  Screen 10:  Random item pickup.  This will be if the player finds a chest on the journey.  This would be an
+//	array, triggered by a randomiser.  Chests will not appear in the same place.  The items that they contain will
 //	also be generated randomly (array).  There will be a very slim chance of getting the ultimate weapon/technique.
 //  This will be decided in the character selection screen (but not explained.  It is specific to each class type).
 
 //	Screen 11:  Store.  The store will be in a randomly, fixed position each game.  It will be a "medieval shop pic"
 //  on a black BG.  There will be a popup window with owner explaining (the first time), how it works.  Accept/ok button
-//  Next pop up will be the items for sale.  They will be general, and available to the entire game, no matter the 
+//  Next pop up will be the items for sale.  They will be general, and available to the entire game, no matter the
 //  class selected.  There will be a "sell" option too  Mechanics:
 //	Once again a table with an array.
 //	Once an item is selected, a confirmation window will pop up. Once yes is selected, item is appended to hero.
 //	Money mechanic will adjust accordingly.
-//	If sell is selected, then hero inventory will appear, and items selected.  A confirmation will appear once 
+//	If sell is selected, then hero inventory will appear, and items selected.  A confirmation will appear once
 //	items have been selected.
 //	Once transactions have been completed, "goodbye" window will appear from shop owner.  Accept and cancel buttons
 //  will be in it.  Accept leads outside, while cancel brings up buy/sell options again.
 
 //  Screen 12:  After achieving a certain experience level, a pop up window will appear saying the hero is ready
-//  to progress.  Following map, hero will fight main boss.  Semi-transparent window on main game screen BG.  
-//	There will be a slight darkening of entire screen to imply seriousness    
+//  to progress.  Following map, hero will fight main boss.  Semi-transparent window on main game screen BG.
+//	There will be a slight darkening of entire screen to imply seriousness
 
 //	Screen 13:  Main boss fight.  Same mechanics as other fights, just longer.  Once boss is defeated, pop up screen
 //	comes up congratulating ultimate master of everything and concluding story.
